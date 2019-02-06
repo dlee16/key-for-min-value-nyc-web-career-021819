7cfe69c58b79fe3f11af6709ba1fc30b8f3ec0b1
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-06 02:32:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ lowest_key = nil
 lowest_value = nil
 
 name_hash.each do |key, value|
-  if value <= lowest_value
+  if value == nil || value< lowest_value
    lowest_value = value 
    lowest_key = key 
   end
