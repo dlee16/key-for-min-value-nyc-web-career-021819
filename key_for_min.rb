e7f2dde30bf0990eaad8b7b32ec05f78259e5cd8
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-06 02:06:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,7 @@ def key_for_min_value(name_hash)
 # name_hash.collect do |key, value|
  min_val < 0
  for item in name_hash
- if item < name_hash[name_hash.index(item)-1] && item < min_val: 
-   min_val= item
+ if item < name_hash[name_hash.index(item)-1] && item < min_val: min_val= item
   puts min_val
 end
 end
